d51a38ab1d681b44f031f891eec4fcbfb5dbeee8
sasha1637 <82914142+sasha1637@users.noreply.github.com>
2023-01-26 01:58:35 +0200

    123

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,55 +1,41 @@
-import React, { Component } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import { Wrapper, Title, Message } from 'components/App.styled';
 import 'react-toastify/dist/ReactToastify.css';
 import ContactForm from 'components/ContactForm/ContactForm';
 import ContactList from 'components/ContactList/ContactList';
 import Filter from 'components/Filter/Filter';
-export class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
-
-  componentDidMount() {
-    const contacts = localStorage.getItem('Contacts');
-    const parsedContacts = JSON.parse(contacts);
-    if (parsedContacts) {
-      this.setState({ contacts: parsedContacts });
-    }
+import { useState, useEffect } from 'react';
+const getInitialContacts = () => {
+  const contacts = localStorage.getItem('Contacts');
+  const parsedContacts = JSON.parse(contacts);
+  if (parsedContacts) {
+    return parsedContacts;
+  } else {
+    return [];
   }
-  componentDidUpdate(_, prevState) {
-    if (prevState.contacts !== this.state.contacts) {
-      localStorage.setItem('Contacts', JSON.stringify(this.state.contacts));
-    }
-  }
-
-  Filter = e => {
+};
+export function App() {
+  const [contacts, setContacts] = useState(getInitialContacts);
+  const [filter, setFilter] = useState('');
+  useEffect(() => {
+    localStorage.setItem('Contacts', JSON.stringify(contacts));
+  }, [contacts]);
+  const FilterContacts = e => {
     const { value } = e.target;
-    this.setState({
-      filter: value,
-    });
+    setFilter(value);
   };
-  getVisibleContacts = () => {
-    const { contacts, filter } = this.state;
+  const getVisibleContacts = () => {
     const normalizeFilter = filter.toLowerCase();
-
     return contacts.filter(contact =>
       contact.name.toLowerCase().includes(normalizeFilter)
     );
   };
-  ContactDelete = id => {
-    const contactDelete = this.state.contacts.find(
-      contact => contact.id === id
-    );
-    this.setState({
-      contacts: this.state.contacts.filter(contact => contact.id !== id),
-    });
+  const ContactDelete = id => {
+    const contactDelete = contacts.find(contact => contact.id === id);
+    setContacts(contacts.filter(contact => contact.id !== id));
     toast.success(`Contact deleted ${contactDelete.name}`);
   };
-
-  AddContact = data => {
-    const { contacts } = this.state;
+  const AddContact = data => {
     const { name, number: NewNumber } = data;
     const normalizeName = name.toLowerCase();
     if (
@@ -62,35 +48,32 @@ export class App extends Component {
         'The subscriber with such contact details is already in the phone book '
       );
     } else {
-      this.setState(({ contacts }) => ({
-        contacts: [data, ...contacts],
-      }));
+      setContacts(Contacts => [data, ...Contacts]);
     }
   };
-  render() {
-    return (
-      <Wrapper>
-        <h1>Phonebook</h1>
-        <ToastContainer />
 
-        <ContactForm onSubmit={this.AddContact}></ContactForm>
-        <Title>Contacts</Title>
-        {this.state.contacts.length ? (
-          <>
-            <Filter value={this.state.filter} onChange={this.Filter} />
-            {this.getVisibleContacts().length ? (
-              <ContactList
-                contacts={this.getVisibleContacts()}
-                ContactDelete={this.ContactDelete}
-              />
-            ) : (
-              <Message>No find contact</Message>
-            )}
-          </>
-        ) : (
-          <Message>No contact</Message>
-        )}
-      </Wrapper>
-    );
-  }
+  return (
+    <Wrapper>
+      <h1>Phonebook</h1>
+      <ToastContainer />
+
+      <ContactForm onSubmit={AddContact}></ContactForm>
+      <Title>Contacts</Title>
+      {contacts.length ? (
+        <>
+          <Filter value={filter} onChange={FilterContacts} />
+          {getVisibleContacts().length ? (
+            <ContactList
+              contacts={getVisibleContacts()}
+              ContactDelete={ContactDelete}
+            />
+          ) : (
+            <Message>No find contact</Message>
+          )}
+        </>
+      ) : (
+        <Message>No contact</Message>
+      )}
+    </Wrapper>
+  );
 }
